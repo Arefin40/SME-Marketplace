a513358011dbea4782bc45c34c6a30472cce5523
ShahriarArefin <arfin17310@gmail.com>
2024-12-03 02:23:23 +0600

    feature: homepage, newsfeed

--- shobai/views.py
+++ shobai/views.py
@@ -1,11 +1,30 @@
+import markdown
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.db import models
+from apps.social.models import Post, PostLike
+from apps.stores.models import Store, StoreFollow
 
 
 @login_required
 def homepage(request):
     """View function for the homepage"""
-    return render(request, "homepage.html")
+    posts = Post.objects.annotate(
+        user_has_liked=models.Exists(
+            PostLike.objects.filter(post=models.OuterRef("pk"), user=request.user)
+        )
+    ).all()
+
+    for post in posts:
+        post.description = markdown.markdown(post.description)
+
+    stores = Store.objects.annotate(
+        is_following=models.Exists(
+            StoreFollow.objects.filter(store=models.OuterRef("pk"), user=request.user)
+        )
+    ).all()
+
+    return render(request, "homepage.html", {"posts": posts, "stores": stores})
 
 
 @login_required
