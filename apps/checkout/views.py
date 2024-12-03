f4daf780e253295ac127cc2b1d2527cce0e1795a
ShahriarArefin <arfin17310@gmail.com>
2024-12-03 12:15:25 +0600

    added order history and order details

--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from apps.users.decorators import role_required
 from apps.products.models import Product
 from .models import CartItem, Order, OrderItem
 from .forms import CheckoutForm
@@ -118,3 +119,28 @@ def get_cart_items_and_total_price(user):
     cart_items = CartItem.objects.filter(user=user)
     total_price = sum(item.get_total_price() for item in cart_items)
     return cart_items, total_price
+
+
+@login_required
+def orders_history(request):
+    user = request.user
+    orders = Order.objects.filter(user=user).order_by("-order_date")
+    return render(request, "orders_history.html", {"orders": orders})
+
+
+@login_required
+def order_details(request, pk):
+    order = Order.objects.get(id=pk)
+
+    if request.user.role == "MERCHANT":
+        order_items = OrderItem.objects.filter(
+            order=order, product__collection__store__merchant=request.user
+        )
+    else:
+        order_items = OrderItem.objects.filter(order=order)
+
+    return render(
+        request,
+        "order_details_admin.html" if request.user.role == "ADMIN" else "order_details.html",
+        {"order": order, "order_items": order_items},
+    )
